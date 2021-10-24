d0aee46950534111d27d2202845c0a061b688482
Kelly <kellytso04@gmail.com>
2021-10-24 17:15:18 -0400

    Added a `favorites` column

--- schema.sql
+++ schema.sql
@@ -6,7 +6,8 @@ DROP TABLE IF EXISTS cows;
 CREATE TABLE cows (
   cow_id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(60) NOT NULL UNIQUE,
-  description VARCHAR(255) NOT NULL
+  description VARCHAR(255) NOT NULL,
+  favorite BOOLEAN
 );
 
 -- mysql -u root -p < schema.sql
